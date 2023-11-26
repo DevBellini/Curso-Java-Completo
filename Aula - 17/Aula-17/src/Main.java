099d1a4f7e978228326e94cca722d8bf23a66f99
Guilherme Bellini <140752834+DevBellini@users.noreply.github.com>
2023-11-25 21:36:34 -0300

    operadores logicos e estrutura condicional

---
+++ Aula - 17/Aula-17/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        OPERADORES COMPARATIVOS
+//        OPERADOR    SIGNIFICADO
+//        >           MAIOR
+//        <           MENOR
+//        >=          MAIOR OU IGUAL
+//        <=          MENOR OU IGUAL
+//        ==          IGUAL
+//        !=          DIFERENTE
+
+//        OPERADORES LÓGICOS
+//        OPERADOR    SIGNIFICADO
+//        &&          E
+//        ||          OU
+//        !           NÃO
+
+//        SINTAXE DA ESTRUTURA CONDICIONAL
+//        SIMPLES:
+//        if(<condição>){
+//            <comando1>
+//            <comando2>
+//        }
+
+//        ENCADEAMENTO DE ESTRUTURAS CONDICIONAIS:
+//        if(<condição>){
+//            <comando1>
+//            <comando2>
+//        }
+//        else{
+//            if(<condição2>){
+////            <comando3>
+////            <comando4>
+//        }
+//        else{
+//               <comando5>
+//               <comando6>
+//            }
+
+
+
+
+    }
+}
